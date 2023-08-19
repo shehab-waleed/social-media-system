e52315cc1fb91a3980e3932059d5b5fdf5b6f458
shehab <shehabwaleed2010@gmail.com>
2023-08-19 09:52:14 +0300

    Adding laravel helper ide package to the project

--- app/Models/PostLike.php
+++ app/Models/PostLike.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\PostLike
+ *
+ * @property int $id
+ * @property int $post_id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Post $post
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PostLike whereUserId($value)
+ * @mixin \Eloquent
+ */
 class PostLike extends Model
 {
     use HasFactory;
