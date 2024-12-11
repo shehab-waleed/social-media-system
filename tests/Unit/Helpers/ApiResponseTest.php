68568bac55085d7af0c0a013a2d41c3c57d841fb
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2024-12-11 21:27:03 +0200

    feat: Wrote unit test for ApiResponse Helper class

---
+++ tests/Unit/Helpers/ApiResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use App\Helpers\ApiResponse;
+use PHPUnit\Framework\TestCase;
+
+class ApiResponseTest extends TestCase
+{
+    public function test_it_should_return_a_valid_response_object_with_valid_data()
+    {
+        $data = [
+            'foo' => 'bar'
+        ];
+
+        $response = ApiResponse::send(
+            200,
+            'This is a success response',
+            $data
+        );
+
+        $this->assertEquals(200 , $response->getStatusCode());
+        $this->assertEquals([
+            'status' => 200,
+            'msg' => 'This is a success response',
+            'data' => $data
+        ], json_decode($response->getContent() , true));
+    }
+
+    public function test_it_should_return_default_response_status_and_data(){
+        $response = ApiResponse::send();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals([
+            'status' => 200,
+            'msg' => null,
+            'data' => null
+        ] , json_decode($response->getContent() , true));
+    }
+}
